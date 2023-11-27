dcb1e68dd55a09a2bcd54ad645dbac9f229a53c8
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2023-11-27 01:51:05 +0000

    square_new

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ get("/howdy") do
 erb( :hello)
 end
 
+get("/square/new") do
+  erb( :square)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("user_number").to_f
+  @the_results = @the_num ** 2
+  
+  erb( :square_results)
+ 
+end
+
 
 get("/") do
   "
